081c15305653075d821e2548dc9ee25371b20078
lFaizaNl <simaosaojose@gmail.com>
2021-03-13 00:56:01 +0000

    comments removed (im under the water please help me)

--- mqttsid/src/main/java/grupo11/mqttsid/Sender.java
+++ mqttsid/src/main/java/grupo11/mqttsid/Sender.java
@@ -1,17 +1,12 @@
 package grupo11.mqttsid;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 import org.apache.commons.lang3.SerializationUtils;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import org.bson.types.ObjectId;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
@@ -23,8 +18,6 @@ import com.mongodb.client.model.Filters;
 public class Sender extends Thread implements Callable<Void> {
 	
 	public static final String TOPIC = "sid_g11_xpexial";
-//	private ObjectId lastID;
-//	private File auxID;
 	
 	private MongoCollection<Document> localCollection;
 	
@@ -37,42 +30,9 @@ public class Sender extends Thread implements Callable<Void> {
 	public Sender(IMqttClient publisher, String sensor, MongoDatabase localDB) {
         this.publisher = publisher;
         this.localCollection = localDB.getCollection("sensor" + sensor);
-//		new File(".\\lastIDs").mkdir();
-//		this.auxID = new File(".\\lastIDs\\lastID_" + sensor + ".txt");
-//		this.lastID = getLastID();
 		this.sqlmanager = new SQLHandler("jdbc:mysql://localhost:3306/projetosid", "root", "");
 		setLastDate();
     }
-	
-//	private void setLastID(Object lastID) {
-//		try {
-//			PrintWriter writer = new PrintWriter(auxID);
-//			writer.print(lastID);
-//			writer.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//	}
-	
-//	private ObjectId getLastID() {
-//		try {
-//			if(!auxID.exists()) {
-//				this.auxID.createNewFile();
-//			}
-//			Scanner scanner = new Scanner(auxID);
-//			if(scanner.hasNext()) {
-//				ObjectId aux = new ObjectId(scanner.next());
-//				scanner.close();
-//				return aux;
-//			} else {
-//				scanner.close();
-//				return null;
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return null;
-//		}
-//	}
 	
 	private void setLastDate() {
 		ResultSet aux = sqlmanager.queryDB("select hora from medicao order by hora DESC limit 1");
@@ -90,8 +50,7 @@ public class Sender extends Thread implements Callable<Void> {
 		
 		FindIterable<Document> sorted = localCollection.find();
 		
-		if(/*lastID != null*/lastDate != null) {
-			//Bson bsonFilter = Filters.gt("_id", lastID);
+		if(lastDate != null) {
 			Bson bsonFilter = Filters.gt("Data", lastDate);
 			sorted = sorted.filter(bsonFilter);
 		}
@@ -103,13 +62,10 @@ public class Sender extends Thread implements Callable<Void> {
 					payload = SerializationUtils.serialize(d);
 					call();
 					lastDate = d.getString("Data");
-//					lastID = (ObjectId) d.get("_id");
-//					setLastID(lastID);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-//			Bson bsonFilter = Filters.and(Filters.gt("_id", lastID), typeFilter);
 			sorted = sorted.filter(Filters.gt("Date", lastDate));
         }
 	}
